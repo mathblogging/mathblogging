a64fcaf2e1d2e2bc0c713d4307edbace1030cbae
pkra <pkrautzb@umich.edu>
2010-12-16 23:21:41 -0500

    updated the footer

--- main.py
+++ main.py
@@ -57,7 +57,15 @@ menu = """
 
 footer = """
 <!-- copyright footer -->
-<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" property="dct:title" rel="dct:type">mathblogging.org</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>.
+<br />
+<br />
+<center>
+<br /><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/80x15.png" /></a>
+<br /> 
+<p style="width: 33%; margin: auto;font-size: 60%; color: gray; text-align: center">
+<br /><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" property="dct:title" rel="dct:type">mathblogging.org</span> is licensed under a <br /> <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>.
+</p>
+</center>
 <!-- end copyright footer -->
 """
 
