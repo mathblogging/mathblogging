a8ab6f18897440eda4449f89d05b2050160f4e26
Peter Krautzberger <peter@boston.(none)>
2010-12-16 22:56:22 -0500

    creative commons footer added to main.py

--- main.py
+++ main.py
@@ -55,6 +55,13 @@ menu = """
 <!-- end Top Navigation -->
 """
 
+footer = """
+<!-- copyright footer -->
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" property="dct:title" rel="dct:type">mathblogging.org</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>.
+<!-- end copyright footer -->
+"""
+
+
 class Feed(db.Model):
     url = db.LinkProperty()
     homepage = db.StringProperty()
