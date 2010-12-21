187f0d2fc8cd61f83c30abab818522320e88d1ab
Felix Breuer <felix@fbreuer.de>
2010-12-21 18:10:46 +0100

    AGPL license added

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+# Mathblogging is a simple blog aggregator.
+# Copyright (C) 2010 Felix Breuer, Frederik von Heymann, Peter Krautzberger
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from Cheetah.Template import Template
 
 import wsgiref.handlers
