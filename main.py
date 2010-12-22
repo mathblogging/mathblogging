84dd45fb02d1db72fd33f868633b73ba4260800c
Felix Breuer <felix@fbreuer.de>
2010-12-22 18:20:15 +0100

    limit of 1000 entries in date view

--- main.py
+++ main.py
@@ -241,7 +241,7 @@ class DateView(webapp.RequestHandler):
     def get(self):
         all_entries = [ entry for feed in Feed.all().filter("type !=","micro").filter("type !=","community") for entry in feed.entries() ]
         all_entries.sort( lambda a,b: - cmp(a.timestamp,b.timestamp) )
-        template_values = { 'qf':  QueryFactory(), 'allentries': all_entries, 'menu': menu, 'footer': footer, 'disqus': disqus }
+        template_values = { 'qf':  QueryFactory(), 'allentries': all_entries[0:1000], 'menu': menu, 'footer': footer, 'disqus': disqus }
     
         path = os.path.join(os.path.dirname(__file__), 'bydate.tmpl')
         self.response.out.write(Template( file = path, searchList = (template_values,) ))
