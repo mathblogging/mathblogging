4fad729ff77a8bdcf3e9e9a72c4e8b57e58d512f
Felix Breuer <felix@fbreuer.de>
2011-03-30 00:34:54 +0200

    clean up merge mess

--- main.py
+++ main.py
@@ -470,11 +470,7 @@ class FeedHandlerCommunities(webapp.RequestHandler):
 
 class FeedHandlerPeople(webapp.RequestHandler):
     def get(self):
-<<<<<<< HEAD
-        all_entries = [ entry for feed in Feed.all().filter("type !=","communities").filter("type !=","institutions") for entry in feed.entries() ]
-=======
-        all_entries = [ entry for feed in Feed.all().filter("type !=","community").filter("type !=","institution") for entry in feed.entries() ]
->>>>>>> 451b35d1841cee8dfc8038616dfb5451dc69476e
+        all_entries = [ entry for feed in Feed.all().filter("type !=","community").filter("type !=","institutions") for entry in feed.entries() ]
         all_entries.sort( lambda a,b: - cmp(a.timestamp,b.timestamp) )
         template_values = { 'qf':  QueryFactory(), 'allentries': all_entries[0:150], 'menu': menu, 'disqus': disqus, 'header': header }
     
@@ -483,11 +479,7 @@ class FeedHandlerPeople(webapp.RequestHandler):
         
 class FeedHandlerAcademics(webapp.RequestHandler):
     def get(self):
-<<<<<<< HEAD
-        all_entries = [ entry for feed in Feed.all().filter("type !=","communities").filter("type !=","educator").filter("type !=","journalism") for entry in feed.entries() ]
-=======
         all_entries = [ entry for feed in Feed.all().filter("type !=","community").filter("type !=","educator").filter("type !=","journalism") for entry in feed.entries() ]
->>>>>>> 451b35d1841cee8dfc8038616dfb5451dc69476e
         all_entries.sort( lambda a,b: - cmp(a.timestamp,b.timestamp) )
         template_values = { 'qf':  QueryFactory(), 'allentries': all_entries[0:150], 'menu': menu, 'disqus': disqus, 'header': header }
     
