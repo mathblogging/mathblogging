1335ed65b59fbff1e56c617f8a7777de47fde0a8
Felix Breuer <felix@fbreuer.de>
2011-08-11 10:17:23 -0700

    StatsView errors fixed

--- statsview.py
+++ statsview.py
@@ -5,28 +5,27 @@ class StatsView(TemplatePage):
     def generateContent(self):
        output = []
        output.append("""
-       <div class="content">
-           <table class="bydate" id="commentsperday">
-              <thead>
-                 <tr>
-                    <th align="left" class="datecolumn">
-                    </th>
-                    <th align="left" class="blogcolumn">
-                       Comments last 24h
-                    </th>
-                 </tr>
-              </thead>
-              <tbody>""")
+            <table class="bydate" id="commentsperday">
+                <thead>
+                    <tr>
+                        <th align="left" class="datecolumn">
+                        </th>
+                        <th align="left" class="blogcolumn">
+                            Comments last 24h
+                        </th>
+                    </tr>
+                </thead>
+            <tbody>""")
        for feed in Feed.gql("WHERE category IN :1 ORDER BY comments_day DESC", ['history','fun','general','commercial','art','visual','pure','applied','teacher','journalism']):
           output.append("""
                  <tr>
                     <td valign="bottom" class="datecolumn">
-                       %(comments_day)i
+                       %(comments_day)s
                     </td>
                     <td valign="bottom" class="blogcolumn">
                        <a href="%(homepage)s">%(title)s</a>
                     </td>
-                 </tr>""" % {'comments_day': feed.comments_day, 'homepage': feed.homepage, 'title': feed.title })
+                 </tr>""" % {'comments_day': str(feed.comments_day), 'homepage': feed.homepage, 'title': feed.title })
        output.append("""
               </tbody>
            </table>""")
@@ -47,12 +46,12 @@ class StatsView(TemplatePage):
           output.append("""
                  <tr>
                     <td valign="bottom" class="datecolumn">
-                       %(comments_week)i
+                       %(comments_week)s
                     </td>
                     <td valign="bottom" class="blogcolumn">
                        <a href="%(homepage)s">%(title)s</a>
                     </td>
-                 </tr>""" % {'comments_week': feed.comments_week, 'homepage': feed.homepage, 'title': feed.title })
+                 </tr>""" % {'comments_week': str(feed.comments_week), 'homepage': feed.homepage, 'title': feed.title })
        output.append("""
               </tbody>
            </table>""")
@@ -69,16 +68,16 @@ class StatsView(TemplatePage):
                  </tr>
               </thead>
               <tbody>""")
-       for feed in Feed.gql("WHERE category IN :1 ORDER BY posts_day DESC", ['history','fun','general','commercial','art','visual','pure','applied','teacher','journalism']):
+       for feed in Feed.gql("WHERE category IN :1 ORDER BY posts_week DESC", ['history','fun','general','commercial','art','visual','pure','applied','teacher','journalism']):
           output.append("""
                  <tr>
                     <td valign="bottom" class="datecolumn">
-                       %(posts_week)i
+                       %(posts_week)s
                     </td>
                     <td valign="bottom" class="blogcolumn">
                        <a href="%(homepage)s">%(title)s</a>
                     </td>
-                 </tr>""" % {'posts_week': feed.posts_week, 'homepage': feed.homepage, 'title': feed.title })
+                 </tr>""" % {'posts_week': str(feed.posts_week), 'homepage': feed.homepage, 'title': feed.title })
        output.append("""
               </tbody>
            </table>""")
@@ -95,16 +94,16 @@ class StatsView(TemplatePage):
                  </tr>
               </thead>
               <tbody>""")
-       for feed in Feed.gql("WHERE category IN :1 ORDER BY posts_day DESC", ['history','fun','general','commercial','art','visual','pure','applied','teacher','journalism']):
+       for feed in Feed.gql("WHERE category IN :1 ORDER BY posts_month DESC", ['history','fun','general','commercial','art','visual','pure','applied','teacher','journalism']):
           output.append("""
                  <tr>
                     <td valign="bottom" class="datecolumn">
-                       %(posts_month)i
+                       %(posts_month)s
                     </td>
                     <td valign="bottom" class="blogcolumn">
                        <a href="%(homepage)s">%(title)s</a>
                     </td>
-                 </tr>""" % {'posts_month': feed.posts_month, 'homepage': feed.homepage, 'title': feed.title })
+                 </tr>""" % {'posts_month': str(feed.posts_month), 'homepage': feed.homepage, 'title': feed.title })
        output.append("""
               </tbody>
            </table>""")
