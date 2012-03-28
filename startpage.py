beedffbb821cf6c05529f512ad83cbe5453b315f
Felix Breuer <felix@fbreuer.de>
2012-03-28 11:36:23 -0700

    add google analytics to template and startpage

--- startpage.py
+++ startpage.py
@@ -30,6 +30,16 @@ a {
 }
     </style>
     <title>Mathblogging.org</title>
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-30403585-1']);
+      _gaq.push(['_trackPageview']);
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
   </head><body>
 <h1 style="text-align:center">Mathblogging.org <span style="font-size: 10pt; color: rgb(204, 204, 204);">your one-stop shop for mathematical blogs</span></h1>
 
